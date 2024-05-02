face7a52c0ddc70ad21acd5de2407f0856d55ad9
akephong <moomoohorn@gmail.com>
2024-05-02 23:30:12 +0700

    update code.

--- Internal-Dashboard/internal-dashboard-app.js
+++ Internal-Dashboard/internal-dashboard-app.js
@@ -3,5 +3,24 @@ app.controller(
   "InternalDashboardPageCtrl",
   ($scope, $filter, $timeout, $http) => {
     $scope.leftmenu = "../view/partial/leftmenu.html";
+    $scope.BoxDisplayOne = "../view/partial/boxStyleOne.html";
+
+    $scope.inventorys = [
+      {
+        title: "Expexted Orders",
+        count: 2,
+        link: "//google.com",
+      },
+      {
+        title: "Expexted Orders",
+        count: 5,
+        link: "//google.com",
+      },
+      {
+        title: "Expexted Orders",
+        count: 75,
+        link: "//google.com",
+      },
+    ];
   }
 );
